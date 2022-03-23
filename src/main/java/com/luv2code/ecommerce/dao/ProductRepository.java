be4ccee44020eb1c63422525faa81e45dde8497a
htanivar <37423501+htanivar@users.noreply.github.com>
2022-03-24 00:59:57 +0530

    CORS: CrossOrigin annotation added (#2)

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -3,8 +3,10 @@ package com.luv2code.ecommerce.dao;
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
