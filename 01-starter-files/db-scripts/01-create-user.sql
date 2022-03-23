d0635ed080ff01e8af824fb7ca0e21eb4e9ddae3
htanivar <37423501+htanivar@users.noreply.github.com>
2022-03-23 21:05:18 +0530

    Retrieved Product & Product Category API

---
+++ 01-starter-files/db-scripts/01-create-user.sql
@@ -0,0 +1,19 @@
+create database ecommerceapp;
+
+use ecommerceapp;
+
+CREATE USER 'ecommerceapp'@'localhost' IDENTIFIED BY 'yourpassword';
+
+GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
+
+#
+# Starting with MySQL 8.0.4, the MySQL team changed the 
+# default authentication plugin for MySQL server 
+# from mysql_native_password to caching_sha2_password.
+#
+# The command below will make the appropriate updates for your user account.
+#
+# See the MySQL Reference Manual for details: 
+# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
+#
+ALTER USER 'ecommerceapp'@'%' IDENTIFIED WITH mysql_native_password BY 'yourpassword';
